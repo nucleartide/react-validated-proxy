719696616e6a670f96a5a86e26551b519b722bf7
Jason Tu <jasontu4@gmail.com>
2018-03-24 17:15:57 -0400

    Add index.tsx

---
+++ index.tsx
@@ -0,0 +1,17 @@
+console.log('hello world');
+
+import * as React from 'react';
+
+interface Person {
+  foo: string;
+  bar: string;
+}
+
+let p: Person = {
+  foo: 42,
+  bar: '',
+}
+
+const SomeComponent = () => (
+  <div>hello world</div>
+);
